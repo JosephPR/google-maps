26e9c24c42ae04c09dfa06083803e7daaefb74c3
JosephPR <reilly4181@gmail.com>
2019-09-18 23:02:18 -0600

    park data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker, InfoWindow } from 'react-google-maps'
+import  * as parksData from './Data/parks.json'
 
-function App() {
+function Map() {
+  const [selectedPark, setSelectedPark] = useState(null);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat:39.791408, lng:-104.967928 }}
+      >
+      {parksData.parks.map(park => {
+        return (
+          <Marker
+             key={park.LOC_CODE}
+             position={{ lat: park.LATITUDE, lng: park.LONGITUDE}}
+             onClick={() => {
+               setSelectedPark(park);
+             }}
+             icon={{
+               
+             }}
+             />
+
+        )
+      })}
+      {selectedPark && (
+        <InfoWindow
+             onCloseClick={() => {
+               setSelectedPark(null);
+             } }
+             position={{ lat: selectedPark.LATITUDE, lng: selectedPark.LONGITUDE}}
+          >
+          <div>
+            <h2>{selectedPark.FORMAL_NAME}</h2>
+            <p>{selectedPark.ADDRESS_LINE1}</p>
+            <h4>Features</h4>
+            <p>{selectedPark.FACILITIES}</p>
+          </div>
+        </InfoWindow>
+      )}
+    </GoogleMap>
+    </>
+  )
 }
 
-export default App;
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
+export default function App() {
+  return (
+    <>
+      <div style={{width: "100vw",height: "100vh"}}>
+      <WrappedMap
+         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyARoc1Kn3hl78tPT8zdKP9ucnZIOsw0r0Q`}
+        loadingElement={<div style={{ height: "100%"}} />}
+        containerElement={<div style={{ height: "100%"}} />}
+        mapElement={<div style={{ height: "100%"}} />}
+        />
+      </div>
+    </>
+   );
+}
